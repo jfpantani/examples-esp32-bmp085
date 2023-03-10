9f393be9a00eb2ba1f219e7d349b0a9c50d9f46b
Joao F. Pantani <jfpantani@gmail.com>
2023-03-09 22:03:18 -0300

    Some changes and additions

--- main/bmp085.c
+++ main/bmp085.c
@@ -3,7 +3,7 @@
    Simple I2C example that shows how to initialize I2C
    as well as reading and writing from and to registers for a sensor connected over I2C.
 
-   The sensor used in this example is a MPU9250 inertial measurement unit.
+   The sensor used in this example is a BMP085 pressure sensor unit.
 
    For other examples please check:
    https://github.com/espressif/esp-idf/tree/master/examples
